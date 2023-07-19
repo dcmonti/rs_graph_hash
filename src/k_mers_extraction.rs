b1a0b49d7044f8aa54ece180e1587aeab7190ad1
nada0 <n.mohamed3@campus.unimib.it>
2023-07-19 15:53:32 +0200

    kmer

--- src/k_mers_extraction.rs
+++ src/k_mers_extraction.rs
@@ -1,44 +1,59 @@
-use std::vec;
+use std::{vec, collections::VecDeque};
+
+use bit_vec::BitVec;
 
 use crate::graph_parser::PathGraph;
 
+
 pub fn extract_kmers(graph: &PathGraph, k: usize) {
-    //POSITIONS DEVE ESSERE UNA CODA
-    let graph_len = graph.lnz.len();
     let starting_positions = graph.succ_hash.get_succs_and_paths(0);
-    let mut positions = vec![];
+    let mut positions = VecDeque::new();
+    let mut visited_nodes = BitVec::from_elem(*graph.nodes_id_pos.iter().max().unwrap() as usize + 1, false);
     for (path_start, _) in starting_positions {
-        positions.push(path_start);
+        if !positions.contains(&path_start) {
+            positions.push_front(path_start);
+            
+        }
     }
         
-    for window_pos in &mut positions {
+    while !positions.is_empty() {
+        let window_pos = positions.pop_front().unwrap();
+        if !visited_nodes[graph.nodes_id_pos[window_pos] as usize]{
+            
             let mut kmer = vec![];
-            recursive_extraction(graph, &mut kmer, k, *window_pos);
-            println!("{:?}", window_pos);
+            recursive_extraction(graph, &mut kmer, k, window_pos);
 
-            if graph.nws[*window_pos] {
-
+            if graph.nws[window_pos] {
+                visited_nodes.set(graph.nodes_id_pos[window_pos] as usize, true);
+                for (succ, _) in graph.succ_hash.get_succs_and_paths(window_pos){
+                    if graph.lnz[succ] != 'F' {
+                        positions.push_front(succ);
+                    }
+                }
             } else {
-                positions.push(*window_pos + 1)
+                positions.push_front(window_pos+1)
             }
+        }
+        
 
     
     }
 
     }
 
-    fn recursive_extraction(graph: &PathGraph, kmer: &mut Vec<char>, k: usize, mut idx: usize) {
-        
-        kmer.push(graph.lnz[idx]);
-        while kmer.len() < k && idx < graph.lnz.len() - 1 {
+    fn recursive_extraction(graph: &PathGraph, kmer: &Vec<char>, k: usize, idx: usize) {
+        let mut loc_kmer = kmer.clone();
+        loc_kmer.push(graph.lnz[idx]);
+        if loc_kmer.len() < k && idx < graph.lnz.len() - 1 {
             
             if !graph.nws[idx] {
-                idx += 1;
-                kmer.push(graph.lnz[idx]);
+                recursive_extraction(graph, &mut loc_kmer, k, idx+1);
+
             } else {
                 for (succ_idx, _) in graph.succ_hash.get_succs_and_paths(idx) {
                     if graph.lnz[succ_idx] != 'F' {
-                        recursive_extraction(graph, kmer, k, succ_idx);
+
+                        recursive_extraction(graph, &mut loc_kmer, k, succ_idx);
                     } else {
                         return;
                     }
@@ -46,5 +61,11 @@ pub fn extract_kmers(graph: &PathGraph, k: usize) {
                
             }
         }
-        println!("{:?}", kmer)
+        if loc_kmer.len() == k {
+            println!("{:?}", loc_kmer);
+
+        }
+
+        //println!("{:?}", idx);
+        
     }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use rs_graph_hash::graph_parser;
 use rs_graph_hash::k_mers_extraction;
+use std::collections::VecDeque;
+
 fn main() {
     let graph = graph_parser::read_graph_w_path("./prova.gfa", false);
 
     k_mers_extraction::extract_kmers(&graph, 3);
-    graph.to_string();
+    //graph.to_string();
+
+    
 }
