d6ceeb4d63b8cb21c8dad0249497f230a268e9b0
dcmonti <d.monti11@campus.unimib.it>
2023-07-20 12:30:42 +0200

    debug prints

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,18 @@ fn main() {
     let graph = io_parser::read_graph_w_path("./graph.gfa", false);
     let read = io_parser::read_sequence_w_path("./read.fa");
     let unique_kmers = k_mers_extraction::extract_graph_unique_kmers(&graph, k);
-    let candidates_kmers = k_mers_extraction::filter_read_kmers(&read, &unique_kmers, k);
+
+    println!("UNIQUE KMERS");
+    for (k, v) in unique_kmers.iter() {
+        println!("KMER: {:?}\tPATHS: {:?}", k, v);
+    }
     
+    let candidates_kmers = k_mers_extraction::filter_read_kmers(&read, &unique_kmers, k);
+    println!("");
+    println!("CANDIDATES KMERS");
+    for (k, v) in candidates_kmers.iter() {
+        println!("POS: {:?}\tPATHS: {:?}", k, v);
+    }
     find_recomb_kmers(&candidates_kmers);
     
     /*
